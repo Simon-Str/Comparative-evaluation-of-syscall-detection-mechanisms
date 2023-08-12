8353eb051c2088b2174da956270c87f8b846da1a
Simon Stromberg <simon.stromberg@gmx.de>
2023-08-12 17:55:30 +0200

    safety commit bc local cleanup

--- vagrant/provisioning/run_command.sh
+++ vagrant/provisioning/run_command.sh
@@ -33,5 +33,6 @@ spa -condition-cfg $BASEDIR/bitcodes/$BC_NAME 2>&1 | tee $BASEDIR/callgraphs/$BI
 spa -simple $BASEDIR/bitcodes/$BC_NAME 2>&1 | tee $BASEDIR/callgraphs/$BINARY.svf.function.pointer.allocations.wglobal.cfg
 python3 $BASEDIR/python-utils/graphCleaner.py --fpanalysis --funcname main --output $BASEDIR/callgraphs/$BINARY.svf.new.type.fp.wglobal.cfg --directgraphfile $BASEDIR/callgraphs/$BINARY.svf.conditional.direct.calls.cfg --funcpointerfile $BASEDIR/callgraphs/$BINARY.svf.function.pointer.allocations.wglobal.cfg -c $BASEDIR/callgraphs/$BINARY.svf.type.cfg
 mkdir -p $BASEDIR/outputs; mkdir -p $BASEDIR/stats
+python3 $BASEDIR/change_appToProperties.py -i $BASEDIR/app.to.properties.json -b $BINARY
 python3 $BASEDIR/createSyscallStats.py -c $BASEDIR/callgraphs/glibc.callgraph --apptopropertymap $BASEDIR/app.to.properties.json --binpath $BASEDIR/binaries/ --outputpath $BASEDIR/outputs/ --apptolibmap $BASEDIR/app.to.lib.map.json --sensitivesyscalls $BASEDIR/sensitive.syscalls --sensitivestatspath $BASEDIR/stats/sensitive.stats --syscallreductionpath $BASEDIR/stats/syscallreduction.stats --libdebloating --othercfgpath $BASEDIR/otherCfgs/ --cfgpath $BASEDIR/callgraphs
 exit 0
